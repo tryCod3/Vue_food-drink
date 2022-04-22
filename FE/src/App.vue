8e053dd11ec584dad7c2335c9b6faa01e78722e9
tryCod3 <nthuy1999kadd1@gmail.com>
2022-04-22 08:49:27 +0700

    master config

---
+++ FE/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+	<div id="app">
+		<h1 class="text-pink-400">configuration tailwindcss</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "App",
+	components: {},
+};
+</script>
+
+<style></style>
